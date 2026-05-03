581b9f772ba5f729141ced153fd8300f4a1ae681
Keagan Rodrigues <keagan123@outlook.com>
2026-05-03 18:39:16 -0400

    Footer section completed

--- constants/index.ts
+++ constants/index.ts
@@ -19,3 +19,22 @@ export const productType = [
     {title: 'Hoodie', value: 'hoodie'},
     {title: 'Short', value: 'short'},
 ];
+
+
+export const quickLinksData = [
+    {title: "About Us", href: "/about"},
+    {title: "Contact Us", href: "/contact"},
+    {title: "Terms & Conditions", href: "/terms"},
+    {title: "Privacy Policy", href: "/privacy"},
+    {title: "FAQs", href: "/faqs"},
+]
+
+export const categoriesData = [
+    {title: "Men's Fashion", href: "/men"},
+    {title: "Women's Fashion", href: "/women"},
+    {title: "Kids corner", href: "/kids"},
+    {title: "Tshirt", href: "/t-shirt"},
+    {title: "Accessories", href: "/accessories"},
+    {title: "Household", href: "/household"},
+    {title: "Others", href: "/others"},
+]
