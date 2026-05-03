49ffa9d1e2785661cada8b3f7e68580c29b6656d
Keagan Rodrigues <keagan123@outlook.com>
2026-05-02 21:13:40 -0400

    Side bar close feature working

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,6 +2,11 @@ import React, { FC } from 'react';
 import { motion } from "motion/react"
 import Logo from './Logo';
 import { X } from 'lucide-react';
+import headerData from '@/constants';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import SocialMedia from './SocialMedia';
+import { useOutsideClick } from '@/hooks/useOutsideClick';
 
 interface SidebarProps {
     isOpen: boolean;
@@ -9,15 +14,32 @@ interface SidebarProps {
 }
 
 const Sidebar: FC<SidebarProps> = ({isOpen, onClose}) => {
+    const pathname = usePathname();
+    const sidebarRef = useOutsideClick<HTMLDivElement>(onClose);
   return (
     <div className={`fixed inset-y-0 left-0 z-50 bg-darkColor/50 shadow-xl hoverEffect w-full ${isOpen ? 'translate-x-0': '-translate-x-full'}`}>
-        <motion.div className='min-w-72 max-w-96 bg-darkColor text-white/90 h-full p-10 border-r border-r-white flex flex-col gap-6'>
+        <div className="absolute inset-0 bg-darkColor/50" onClick={onClose}>
+
+       
+        <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{duration: 0.4, delay: 0.3}} className='min-w-72 max-w-96 bg-darkColor text-white/70 h-full p-10 border-r border-r-white flex flex-col gap-6'>
             <div className='flex items-center justify-between'>
-                <Logo className='text-white'>Tulos</Logo>
+                <button onClick={onClose}>
+                    <Logo className='text-white'>Tulos</Logo>
+                </button>
                 <button className='hover:text-red-500 hoverEffect' onClick={onClose}><X className=''/></button>
             </div>
+             <div className='flex flex-col gap-3.5 text-base font-semibold tracking-wide'>
+      {headerData?.map((item) => (
+        <Link onClick={onClose} key={item?.title} href={item?.href} className={`hover:text-white hoverEffect w-24 ${pathname === item?.href && 'text-white'}`}>
+          {item?.title}
+        </Link>
+
+        ))}
+          </div>
+          <SocialMedia />
         </motion.div>
     </div>
+     </div>
   )
 }
 
---
+++ components/SocialMedia.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { TooltipProvider, TooltipTrigger, Tooltip, TooltipContent } from './ui/tooltip'
+import Link from 'next/link'
+import { Github, Linkedin, Instagram, Youtube, Facebook, Slack } from "lucide-react"
+import { cn } from '@/lib/utils'
+// import { Tooltip } from 'radix-ui'
+
+interface Props {
+    className?: string,
+    iconClassName?: string, // icon name
+    tooltipClassName?: string, // hover text + hv text bg
+}
+
+const socialLink = [
+    {
+        title: "Youtube",
+        href: "https://www.youtube.com/JMB12",
+        target: "_blank",
+        icon: <Youtube className='w-5 h-5'/>
+    },
+    {
+        title: "Github",
+        href: "https://github.com/JMBoulos12",
+        target: "_blank",
+        icon: <Github className='w-5 h-5'/>
+    },
+    {
+        title: "LinkedIn",
+        href: "https://linkedin.com/in/jmboulos/",
+        target: "_blank",
+        icon: <Linkedin className='w-5 h-5'/>
+    },
+    {
+        title: "Facebook",
+        href: "https://www.youtube.com/@reactjsBD",
+        target: "_blank",
+        icon: <Facebook className='w-5 h-5'/>
+    },
+    {
+        title: "Slack",
+        href: "https://www.youtube.com/@reactjsBD",
+        target: "_blank",
+        icon: <Slack className='w-5 h-5'/>
+    },
+]
+
+function SocialMedia({className, iconClassName, tooltipClassName}: Props) {
+  return ( <TooltipProvider>
+    <div className={cn('flex items-center gap-3.5', className)}>
+        {socialLink?.map((item) => (
+            <Tooltip key={item?.title}>
+            <TooltipTrigger asChild>
+                <Link href={item?.href} target={item?.target} rel='noopener noreferrer' className={cn('p-2 border rounded-full hover:text-white hover:border-white hoverEffect', iconClassName)}>
+                    {/* <Github />  */}
+                    {item?.icon}
+                </Link>
+            </TooltipTrigger>
+            <TooltipContent className={cn('bg-white text-darkColor font-semibold', tooltipClassName)}>
+                {item?.title}
+            </TooltipContent>
+        </Tooltip>
+        ))}
+    </div>
+  </TooltipProvider>
+  )
+}
+
+export default SocialMedia
