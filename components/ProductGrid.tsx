e1b44e5928b1aba585e2bf57bbf293aa8e4da104
Keagan Rodrigues <keagan123@outlook.com>
2026-05-03 15:21:48 -0400

    Before type gen command

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -1,14 +1,41 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import HomeTabBar from './ui/HomeTabBar'
 import { productType } from '@/constants';
+import { client } from '@/sanity/lib/client';
 
 function ProductGrid() {
     const [selectedTab, setSelectedTab] = useState(productType[0]?.title || "");
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const query = `*[_type == 'product' && variant == $variant] | order(name asc)`;
+    const params = {variant: selectedTab.toLocaleLowerCase()};
+    useEffect(() => {
+        const fetchData = async() => {
+            setLoading(true)
+            try {
+                const response = await client.fetch(query, params);
+                setProducts(await response);
+            }
+            catch(error) {
+                console.log("Product Fetch error: ", error);
+            }
+            finally {
+                setLoading(false);
+            }
+        }
+        fetchData();
+    }, [selectedTab]);
 
   return (
     <div className='mt-10 flex flex-col items-center'>
         <HomeTabBar selectedTab={selectedTab} onTabSelect={setSelectedTab} />
+        {loading ? (
+            <div><span>Product is loading...</span></div>
+        ) : (products?.map((item) => (
+            <p key={item?.id}>{item?.name}</p>)
+        ))}
     </div>
   )
 }
