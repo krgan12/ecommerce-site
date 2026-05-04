a77e9b4281d7d1d95272b54f3ca4c47774e2f34c
Keagan Rodrigues <keagan123@outlook.com>
2026-05-04 09:18:22 -0400

    Zenduad stuff added

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,7 +17,7 @@ async function Header() {
   console.log(user);
 
   return (
-    <header className='border-b border-g-gray-400 py-5'>
+    <header className='border-b border-g-gray-400 py-5 sticky top-0 z-50 bg-white'>
         <Container className='flex items-center justify-between gap-7 text-lightColor'>
             <HeaderMenu />
             <div className="w-auto md:w-1/3 flex items-center justify-center gap-2.5">
