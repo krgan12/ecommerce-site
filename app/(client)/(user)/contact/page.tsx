6fc5ef7b0264008d69d7fcaa147274eb565ddc94
Keagan Rodrigues <keagan123@outlook.com>
2026-05-04 08:12:42 -0400

    New Pages Added

--- app/(client)/(user)/contact/page.tsx
+++ app/(client)/(user)/contact/page.tsx
@@ -1,10 +1,32 @@
 import Container from '@/components/Container'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Textarea } from '@/components/ui/textarea'
 import React from 'react'
 
 function ContactPage() {
   return (
-    <Container>
-        Contact Page
+    <Container className='max-w-3xl px-4 sm:px-6 lg:px-8 py-12'>
+        <h1 className='text-3xl font-bold mb-6'>Contact Us</h1>
+        <p className="mb-6">
+          We&apos;d love to hear from you. Please fill out the form below and
+          we&apos;ll get back to you as soon as possible.
+        </p>
+        <form className='space-y-4'>
+          <div className='space-y-2'>
+            <Label htmlFor="name">Name</Label>
+            <Input type='text' name='name' className='w-full px-3 py-2 border border-gray-300 rounded-md' required/>
+          </div>
+          <div className='space-y-2'>
+            <Label htmlFor="name">Email</Label>
+            <Input type='email' name='email' id='email' className='w-full px-3 py-2 border border-gray-300 rounded-md' required/>
+          </div>
+          <div className='space-y-2'>
+            <Label htmlFor="name">Message</Label>
+            <Textarea name='message' id='message' className='w-full px-3 py-2 border border-gray-300 rounded-md resize-none h-40' required/>
+          </div>
+          <button>Send Message</button>
+        </form>
     </Container>
   )
 }
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,11 +1,31 @@
-import { Search } from 'lucide-react'
-import React from 'react'
+'use client'
+import { Search, X } from 'lucide-react'
+import React, { useState } from 'react'
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog'
+import { Input } from './ui/input';
 
 function SearchBar() {
+  const [search, setSearch] = useState("");
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);
   return (
-    <div>
+    <Dialog open={showSearch} onOpenChange={() => setShowSearch(!showSearch)}>
+      <DialogTrigger onClick={() => setShowSearch(!showSearch)}>
         <Search className="w-5 h-5 hover:text-darkColor hoverEffect" />
-    </div>
+      </DialogTrigger>
+      <DialogContent className='max-w-5xl h-[90vh] flex flex-col overflow-hidden rounded-md py-5'>
+        <DialogHeader>
+          <DialogTitle className='mb-1'>Product Searchbar</DialogTitle>
+          <form className='relative ' onSubmit={(e) => e.preventDefault()}>
+            <Input placeholder='Search your product here...' className='flex-1'
+            value={search}
+            onChange={(e) => e.target.value}/>
+            {search && <X className='w-4 h-4 absolute top-3 right-11 hover:text-red-600 hoverEffect'/>}
+          </form>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   )
 }
 
