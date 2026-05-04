f75f5d353cd98eb0d5cbe8d233f0a92356d19485
Keagan Rodrigues <keagan123@outlook.com>
2026-05-04 13:48:46 -0400

    Cart functionality working (almost)

--- app/(client)/cart/page.tsx
+++ app/(client)/cart/page.tsx
@@ -1,12 +1,18 @@
 'use client'
 
+import Container from "@/components/Container";
+import EmptyCart from "@/components/EmptyCart";
 import Loading from "@/components/Loading";
+import NoAccessToCard from "@/components/NoAccessToCard";
+import useCartStore from "@/store";
+import { useAuth, useUser } from "@clerk/nextjs";
 import { useEffect, useState } from "react"
 
 function CartPage() {
   const [isClient, setIsClient] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-
+  const {isSignedIn} = useAuth();
+  const {deleteCartProduct, getTotalPrice, getItemCount, getSubTotalPrice, resetCart, getGroupedItems} = useCartStore();
+  const user = useUser();
   useEffect(() => {
     // 1. Set the timer
     const timer = setTimeout(() => {
@@ -20,8 +26,26 @@ function CartPage() {
   if (!isClient) {
     return <Loading />
   }
+  const cardProducts = getGroupedItems();
+  // console.log(cardProducts);
   return (
-    <div>CartPage</div>
+    <div>
+      {isSignedIn ? (
+        <Container>
+          {cardProducts?.length ? (
+            <>
+              <p>Products</p>
+            </>
+          ) : (
+            <EmptyCart />
+          )}
+        </Container>
+        // <div></div>
+       ) : (
+       <NoAccessToCard />
+       )
+      }
+    </div>
   )
 }
 
