fd6a16f95f70bb010dcc5d5e87db36f6320f6cfa
Keagan Rodrigues <keagan123@outlook.com>
2026-05-05 20:04:06 -0400

    Check out page UI finished

--- app/(client)/cart/page.tsx
+++ app/(client)/cart/page.tsx
@@ -4,13 +4,20 @@ import Container from "@/components/Container";
 import EmptyCart from "@/components/EmptyCart";
 import Loading from "@/components/Loading";
 import NoAccessToCard from "@/components/NoAccessToCard";
+import PriceFormatter from "@/components/PriceFormatter";
+import QuantityBtn from "@/components/QuantityBtn";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import { urlFor } from "@/sanity/lib/image";
 import useCartStore from "@/store";
 import { useAuth, useUser } from "@clerk/nextjs";
-import { ShoppingBag } from "lucide-react";
+import { Heart, ShoppingBag, TrashIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react"
+import { toast } from "sonner";
+import payPalLogo from "@/images/paypalLogo.png"
 
 function CartPage() {
   const [isClient, setIsClient] = useState(false);
@@ -31,7 +38,19 @@ function CartPage() {
     return <Loading />
   }
   const cartProducts = getGroupedItems();
-  // console.log(cardProducts);
+  
+  const handleDeleteProduct= (id: string) => {
+    deleteCartProduct(id);
+    toast(`Product deleted successfully`)
+  }
+  const handleResetCart = () => {
+    const confirmed = window.confirm('Are you sure you want to reset your Cart?');
+    if (confirmed) {
+      resetCart();
+      toast('Your cart reset successfully!');
+    }
+  }
+
   return (
     <div className="bg-gray-50 pb-52 md:pb-10">
       {isSignedIn ? (
@@ -47,6 +66,7 @@ function CartPage() {
               <div className="lg:col-span-2 rounded-lg">
                 <div className="border bg-white rounded-md">
                   {cartProducts?.map(({product}) => {
+                    const itemCount = getItemCount(product?._id);
                     return (
                       <div key={product?._id} className="border-b p-2.5 last:border-b-0 flex items-center justify-between gap-5">
                         <div className="flex flex-1 items-center gap-2 h-36 md:h-44">
@@ -59,21 +79,102 @@ function CartPage() {
                               height={500} 
                               src={urlFor(product?.images[0]).url()} alt="productImage" />
                             </Link>}
+
+                            <div className="h-full flex flex-1 items-start flex-col justify-between py-1">
+                              <div className="space-y-1.5">
+                                <h2 className="font-semibold line-clamp-1">{product?.name}</h2>
+                                <p className="text-sm text-lightColor font-medium">{product?.intro}</p>
+                                <p className="text-sm capitalize">Variant <span className="font-semibold">{product.variant}</span></p>
+                                <p className="text-sm capitalize">Status: <span className="font-semibold">{product?.status}</span></p>
+                              </div>
+                              <div className="text-gray-500 flex items-center gap-2">
+                                <TooltipProvider>
+                                  <Tooltip>
+                                    <TooltipTrigger>
+                                     <Heart className="w-4 h-4 md:w-5 md:h-5 hover:text-green-600 hoverEffect"/>
+                                    </TooltipTrigger>
+                                     <TooltipContent className="font-bold">
+                                        Add to Favourites
+                                      </TooltipContent>
+                                  </Tooltip>
+                                  <Tooltip>
+                                    <TooltipTrigger>
+                                     <TrashIcon onClick={() => {
+                                        handleDeleteProduct(product?._id)
+                                     }} className="w-4 h-4 md:w-5 md:h-5 hover:text-red-600 hoverEffect"/>
+                                    </TooltipTrigger>
+                                     <TooltipContent className="font-bold bg-red-600">
+                                        Delete product
+                                      </TooltipContent>
+                                  </Tooltip>
+                                </TooltipProvider>
+                              </div>
+                            </div>
+                            <div className="flex flex-col items-start justify-between h-36 md:h-44 p-0.5 md:p-1">
+                              <PriceFormatter amount={(product?.price as number) * itemCount}
+                              className="font-bold text-lg"/>
+                              <QuantityBtn product={product} />
+                            </div>
                         </div>
                       </div>
                     )
                   })}
+                  <Button onClick={handleResetCart} className="m-5 font-semibold" variant='destructive'>Reset Cart</Button>
                 </div>
               </div>
               {/* Summary */}
               <div className="lg:col-span-1 ">
                 <div className="hidden md:inline-block w-full bg-white p-6 rounded-lg border">
                   <h2 className="text-xl font-semibold mb-4">Order Summary</h2>
+                  <div className="space-y-4">
+                    <div className="flex justify-between">
+                      <span className="">Subtotal</span>
+                      <PriceFormatter amount={getSubTotalPrice()} />
+                    </div>
+                    <div className="flex justify-between">
+                      <span>Discount</span>
+                      <PriceFormatter amount={getSubTotalPrice() - getTotalPrice()}/>
+                    </div>
+                    <Separator />
+                    <div className="flex justify-between">
+                      <span>Total</span>
+                      <PriceFormatter className="text-lg font-bold text-black" amount={getTotalPrice()}/>
+                    </div>
+                    <Button className="w-full rounded-full font-semibold tracking-wide hoverEffect" size='lg'>Proceed to Checkout</Button>
+                    <Link href={'/'} className="flex items-center justify-center py-2 border border-darkColor/50 rounded-full hover:border-darkColor hover:bg-darkColor/5 hoverEffect">
+                      <Image src={payPalLogo} alt="paypalLogo" className="w-20"/>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              {/* Order summary for mobile view */}
+              <div className="md:hidden fixed bottom-0 left-0 w-full bg-white pt-2">
+                <div className="p-4 rounded-lg border x-4">
+                  <h2 className="text-xl font-semibold mb-4">Order Summary</h2>
+                  <div className="space-y-4">
+                    <div className="flex justify-between">
+                      <span className="">Subtotal</span>
+                      <PriceFormatter amount={getSubTotalPrice()} />
+                    </div>
+                    <div className="flex justify-between">
+                      <span>Discount</span>
+                      <PriceFormatter amount={getSubTotalPrice() - getTotalPrice()}/>
+                    </div>
+                    <Separator />
+                    <div className="flex justify-between">
+                      <span>Total</span>
+                      <PriceFormatter className="text-lg font-bold text-black" amount={getTotalPrice()}/>
+                    </div>
+                    <Button className="w-full rounded-full font-semibold tracking-wide hoverEffect" size='lg'>Proceed to Checkout</Button>
+                    <Link href={'/'} className="flex items-center justify-center py-2 border border-darkColor/50 rounded-full hover:border-darkColor hover:bg-darkColor/5 hoverEffect">
+                      <Image src={payPalLogo} alt="paypalLogo" className="w-20"/>
+                    </Link>
+                  </div>
                 </div>
               </div>
               </div>
             </>
-          ) : (
+          ) : ( 
             <EmptyCart />
           )}
         </Container>
